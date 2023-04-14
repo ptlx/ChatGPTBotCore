9938d9317eaac43f255a663c11663d2495c43204
wurzeit <hiccap.master.jedi@gmail.com>
2023-04-15 00:20:10 +0900

    update

--- ChatGPTBotCore/index.ts
+++ ChatGPTBotCore/index.ts
@@ -45,7 +45,6 @@ const httpTrigger: AzureFunction = async function (
     if (interaction && interaction.type === InteractionType.APPLICATION_COMMAND) {
         const inputMessage = req.body.data.options[0].value
         const username = req.body.member.user.username
-        interaction.deferReply()
         const chatGPTResponse = await ask(inputMessage)
 
         context.res = {
@@ -57,7 +56,7 @@ const httpTrigger: AzureFunction = async function (
             body: JSON.stringify({
                 type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
                 data: {
-                    content: `To ${username}: ${chatGPTResponse}`,
+                    content: 'Please wait...',
                 },
             }),
         }
