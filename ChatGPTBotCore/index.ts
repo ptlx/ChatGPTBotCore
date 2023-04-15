5488e581c4154fc746a42c14a33e68fe41c51958
wurzeit <hiccap.master.jedi@gmail.com>
2023-04-15 22:22:07 +0900

    update

--- ChatGPTBotCore/index.ts
+++ ChatGPTBotCore/index.ts
@@ -45,20 +45,7 @@ const httpTrigger: AzureFunction = async function (
     if (interaction && interaction.type === InteractionType.APPLICATION_COMMAND) {
         const inputMessage = req.body.data.options[0].value
         const username = req.body.member.user.username
-        // context.res = {
-        //     status: 200,
-        //     method: "POST",
-        //     headers: {
-        //         "Content-Type": "application/json",
-        //     },
-        //     body: JSON.stringify({
-        //         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        //         data: {
-        //             content: 'Please wait...',
-        //         },
-        //     }),
-        // }
-        const chatGPTResponse = ask(inputMessage)
+        var test = 'a'
         context.res = {
             status: 200,
             method: "POST",
@@ -68,10 +55,25 @@ const httpTrigger: AzureFunction = async function (
             body: JSON.stringify({
                 type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
                 data: {
-                    content: `${username}: ${chatGPTResponse}`,
+                    content: test,
                 },
             }),
         }
+        test = 'b'
+        // const chatGPTResponse = ask(inputMessage)
+        // context.res = {
+        //     status: 200,
+        //     method: "POST",
+        //     headers: {
+        //         "Content-Type": "application/json",
+        //     },
+        //     body: JSON.stringify({
+        //         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        //         data: {
+        //             content: `${username}: ${chatGPTResponse}`,
+        //         },
+        //     }),
+        // }
     } else {
         context.res = {
             body: JSON.stringify({
@@ -79,6 +81,7 @@ const httpTrigger: AzureFunction = async function (
             }),
         }
     }
+    test = 'b'
 }
 
 export default httpTrigger
